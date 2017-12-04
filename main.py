e37cd44acabdf797307944e5e469df32a2865774
Talha Muhammad <muhammadtalhas@yahoo.com>
2017-12-04 18:31:17 -0500

    added useful shit

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 class Node:
-    adjacent = []
-    colorPicked = None
-    possibleColors = []
+    def __init__(self, name, adjacent=[]):
+        self.name = name
+        self.adjacent = adjacent
+        self.colorPicked = None
+        self.possibleColors = ["R", "B", "G", "Y"]
+
+    def remove_adjacent(self, node):
+        if node in self.adjacent:
+            self.adjacent.remove(node)
+
+    def remove_color(self, color):
+        if color in self.possibleColors:
+            self.possibleColors.remove(color)
+
 
 def main():
-    print("Test")
+    graph = [Node("A", ["B", "C", "D"]),
+             Node("B", ["A", "D"]),
+             Node("C", ["A", "D"]),
+             Node("D", ["A", "B", "C", "E"]),
+             Node("E", ["D"]),
+             Node("F")]
+
 
 if __name__ == "__main__":
     main()
